a390a7a10d1c6207d7fbc2e4fdaaf14b4dc53dbd
Julia Ramos <julia.jrra@gmail.com>
2023-03-10 10:26:50 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDo extends Component {
+  state = {
+    tarefa: "",
+    listaDeTarefas: []
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      tarefa: event.target.value
+    });
+  };
+
+  handleClick = () => {
+    if (this.state.tarefa === "") {
+      return;
+    }
+    this.setState({
+      listaDeTarefas: this.state.listaDeTarefas.concat({
+        tarefa: this.state.tarefa,
+        id: Math.random()
+      }),
+      tarefa: ""
+    });
+  };
+
+  delete = (id) => {
+    this.setState({
+      listaDeTarefas: this.state.listaDeTarefas.filter((item) => {
+        return item.id !== id;
+      })
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="toDoContainer">
+          <div className="blurContainer">
+          <header>
+            <div className-="title">
+            <h1>🌤️Tarefas do dia </h1>
+            </div>
+          </header>
+
+          <div className="inputTarefas">
+            <input
+              placeholder="Digite sua tarefa"
+              onChange={this.handleChange}
+              value={this.state.tarefa}
+            />
+
+            <button className="buttonAdd" onClick={this.handleClick}>
+              {" "}
+              + {" "}
+            </button>
+          </div>
+
+          <div className="listaTarefas">
+            <ul>
+              {this.state.listaDeTarefas.map((item) => (
+                <li>
+                  {" "}
+                  {item.tarefa}
+                  <button
+                    className="buttonRemove"
+                    onClick={() => {
+                      this.delete(item.id);
+                    }}
+                  >
+                  x {""}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App;
+export default ToDo;
